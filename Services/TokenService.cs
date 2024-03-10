227a7950c10ac9d9b9072ff6b72b0188502aa2d0
burakmey <burak_meydan-gs@hotmail.com>
2024-03-10 19:45:14 +0300

    CreateRefreshToken function added. Token instance modified.

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace discord_back_end.Services
@@ -7,23 +8,32 @@ namespace discord_back_end.Services
     public class TokenService(IConfiguration configuration)
     {
         readonly IConfiguration configuration = configuration;
-
         public Token CreateAccessToken(int expirationMinute)
         {
-            Token token = new();
             SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(configuration["SECURITY_KEY"] ?? throw new Exception("SECURITY_KEY not found!")));
             SigningCredentials signingCredentials = new(securityKey, SecurityAlgorithms.HmacSha256);
-            token.Expiration = DateTime.UtcNow.AddMinutes(expirationMinute);
             JwtSecurityToken securityToken = new(
                 audience: configuration["Token:Audience"],
                 issuer: configuration["Token:Issuer"],
-                expires: token.Expiration,
+                expires: DateTime.UtcNow.AddSeconds(expirationMinute),
                 notBefore: DateTime.UtcNow,
                 signingCredentials: signingCredentials
                 );
             JwtSecurityTokenHandler securityTokenHandler = new();
-            token.AccessToken = securityTokenHandler.WriteToken(securityToken);
+            Token token = new()
+            {
+                AccessToken = securityTokenHandler.WriteToken(securityToken),
+                Expiration = DateTime.UtcNow.AddSeconds(expirationMinute),
+                RefreshToken = CreateRefreshToken()
+            };
             return token;
+        }
+        public string CreateRefreshToken()
+        {
+            byte[] numbers = new byte[32];
+            using RandomNumberGenerator random = RandomNumberGenerator.Create();
+            random.GetBytes(numbers);
+            return Convert.ToBase64String(numbers);
         }
     }
 }
